1ac20f5cf0e8e6ff83a62208e7242f858b90828b
billiechen <billiechen1996@gmail.com>
2017-12-05 12:25:26 -0500

    Updates

---
+++ hello_aliens.R
@@ -0,0 +1,7 @@
+cat("Hello there - you are reading a message sent from human beings on",
+    "another planet with similar living environment as yours. We are not",
+    "sure whether you could understand our language, or just stare at a", 
+    "series of symbols and have no idea what it is about. Either way,", 
+    "this will be a defining moment if our message reaches another", 
+    "planet's living creatures. Here are some statistics about our", 
+    "planet: $%&^%$#*&... We look forward to hearing back from you soon!")
